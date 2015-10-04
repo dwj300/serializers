de972e99135a7b7f9327d7053133e8297ffb91a4
dwj300 <dwj300@gmail.com>
2015-10-04 16:00:40 -0400

    making progress

--- serial.c
+++ serial.c
@@ -3,46 +3,113 @@
 
 serial_t* Create_Serial()
 {
-    return NULL;
+    serial_t *serializer = malloc(sizeof(serial_t));
+    serializer->m = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
+    pthread_mutex_init(serializer->m, NULL);
+    return serializer;
 }
 
-Serial_Enter(serial_t* serial)
+void Serial_Enter(serial_t* serial)
 {
-    return NULL;
+    pthread_mutex_lock(serial->m);
+    // have lock
 }
 
-Serial_Exit(serial_t* serial)
+void Serial_Exit(serial_t* serial)
 {
-    return NULL;
+    pthread_mutex_unlock(serial->m);
 }
 
 queue_t* Create_Queue(serial_t* serial)
 {
-    return NULL;
+    // maybe call serial enter??
+    queue_t *queue = malloc(sizeof(queue_t));
+    queue->head = NULL;
+    return queue;
 }
 
 crowd_t* Create_Crowd(serial_t* serial)
 {
-    return NULL;
+    crowd_t *crowd = malloc(sizeof(crowd_t));
+    crowd->head = NULL;
+    return crowd;
 }
 
 int Queue_Empty(serial_t* serial, queue_t* queue)
 {
-    return -1;
+    // check if queue is empty
+    if (queue->head == NULL)
+    {
+        return true;
+    }
+    return false;
 }
 
 int Crowd_Empty(serial_t* serial, crowd_t* crowd)
 {
-    return -1;
+    // check if crowd is empty
+    if (crowd->head == NULL)
+    {
+        return true;
+    }
+    return false;
 }
 
-Serial_Enqueue(serial_t* serial, queue_t* queue, cond_t* func)
+void Serial_Enqueue(serial_t* serial, queue_t* queue, cond_t* func)
 {
-    return NULL;
+    // Add to back of queue
+    // Find first node where condition is true
+    // signal that node
+    // give up serializer lock
+    queue_node_t *temp = queue->head;
+    
+    if (temp == NULL) 
+    {
+        
+        if (func() == true)
+        {
+            return;
+        }
+        temp = malloc(sizeof(queue_node_t));
+        queue->head = temp;
+    }
+    else
+    {
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = malloc(sizeof(queue_node_t));
+        temp = temp->next;
+    }
+    temp->func = func;
+    temp->next = NULL;
+    temp->m = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
+    temp->c = (pthread_cond_t *)malloc(sizeof(pthread_cond_t));
+    pthread_cond_init(temp->c, NULL);
+    
+    queue_node_t *node = queue->head;
+    while(node != NULL)
+    {
+        if (node->func())
+        {
+            printf("hmm");
+        }
+    }
+
+    // wait()
+    Serial_Enter(serial);
+    //pthread_cond_wait()
+    return;
 }
 
-Serial_Join_Crowd(serial_t* serial, crowd_t* crowd, cond_t* func)
+void Serial_Join_Crowd(serial_t* serial, crowd_t* crowd, cond_t* func)
 {
-    return NULL;
+    // already have serializer
+    // join the crowd
+    // give up serializer (serial exit)
+    // call the function
+    // serial_enter
+    return;
 }
 
--- serial.h
+++ serial.h
@@ -1,25 +1,50 @@
-typedef struct {
+#include <pthread.h>
+#include <stdlib.h>
 
+#define true 1
+#define false 0 
+
+typedef int (cond_t)();
+
+typedef struct serial 
+{
+    pthread_mutex_t *m;
 } serial_t;
 
-typedef struct {
+typedef struct queue_node
+{
+    pthread_cond_t *c;
+    pthread_mutex_t *m;
+    cond_t *func;
+    struct queue_node *next;
+} queue_node_t;
 
+typedef struct queue
+{
+    queue_node_t *head;
 } queue_t;
 
-typedef struct {
+typedef struct crowd_node
+{
+    pthread_t p;
+    struct crowd_node *next;
+} crowd_node_t;
 
+typedef struct crowd 
+{
+    crowd_node_t *head;
 } crowd_t;
 
-typedef struct {
+typedef int bool;
 
-} cond_t;
+
 
 serial_t* Create_Serial();
-Serial_Enter(serial_t*);
-Serial_Exit(serial_t*);
+void Serial_Enter(serial_t*);
+void Serial_Exit(serial_t*);
 queue_t* Create_Queue(serial_t*);
 crowd_t* Create_Crowd(serial_t*);
 int Queue_Empty(serial_t* serial, queue_t* queue);
 int Crowd_Empty(serial_t* serial, crowd_t* crowd);
-Serial_Enqueue(serial_t* serial, queue_t* queue, cond_t *func);
-Serial_Join_Crowd(serial_t* serial, crowd_t* crowd, cond_t* func);
+void Serial_Enqueue(serial_t* serial, queue_t* queue, cond_t *func);
+void Serial_Join_Crowd(serial_t* serial, crowd_t* crowd, cond_t* func);
